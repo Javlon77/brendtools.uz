5d4928ca281df60c8dc6b90ffc5738997348ff88
Office <you@example.com>
2022-03-10 18:49:48 +0500

    version for main.css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     @yield('css')
-    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/main.css?ver=0.1" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 <body>
